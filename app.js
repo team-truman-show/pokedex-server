8ad7c06d0982dbf27dcd733a4041737c92373177
gusn98 <ah529@naver.com>
2023-06-13 22:13:39 +0900

    비밀번호검증코드 추가

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-
-const express = require('express');
-const models = require('./models/index');
-const loginRouter = require('./service/login')
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const models = require("./models/index");
+const loginRouter = require("./service/login");
+const signupRouter = require("./service/signup");
+const bodyParser = require("body-parser");
 
 const app = express();
 // body-parser 미들웨어 설정
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", loginRouter);
+app.use("/", signupRouter);
 
-models.sequelize.authenticate().then(() => {
-    models.sequelize.sync().then(()=> {
-        console.log('Sequelize sync success');
-    }).catch((err) => {
-        console.error('Sequelize sync failed',err);
-    })
-}).catch((err) => {
-    console.error('DB connection fail',err);
-})
+models.sequelize
+  .authenticate()
+  .then(() => {
+    models.sequelize
+      .sync()
+      .then(() => {
+        console.log("Sequelize sync success");
+      })
+      .catch((err) => {
+        console.error("Sequelize sync failed", err);
+      });
+  })
+  .catch((err) => {
+    console.error("DB connection fail", err);
+  });
 
-app.use('/',loginRouter);
-
-app.listen(3000,() => {
-    console.log('3000번 포트 대기중');
+app.listen(3000, () => {
+  console.log("3000번 포트 대기중");
 });
