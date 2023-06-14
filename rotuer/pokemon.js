e54a964c0ba1e875a08fcbb445b8590dc39bd353
j.b.lim <j.b@jblimui-MacBookAir.local>
2023-06-14 19:37:30 +0900

    add: 이름으로 포켓몬 찾기

--- rotuer/pokemon.js
+++ rotuer/pokemon.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const pokeapi = require('../service/pokeapi');
 const Pokemon = require('../models/pokemons');
 //api 불러오기,id 검색
-router.post('/api',(req,res) =>{
-    pokeapi(req.body.id)
+router.post('/idsearch',(req,res) =>{
+    pokeapi.Pokeidsearch(req.body.id)
   .then(result => {
     res.status(200).send(result);
   })
@@ -16,22 +16,28 @@ router.post('/api',(req,res) =>{
 async function save() {
     for (let i = 1; i <= 151; i++) {
       try {
-        const result = await pokeapi(i);
+        const result = await pokeapi.Pokeidsearch(i);
         console.log(result.name);
         await Pokemon.create({
             id: i,
-          name: result.name,
-          feature: result.feature,
-          description: result.description,
-          type1: result.type1,
-          type2: result.type2,
-          imageurl: result.imageurl
+            name: result.name,
+            feature: result.feature,
+            description: result.description,
+            type1: result.type1,
+            type2: result.type2,
+            imageurl: result.imageurl
          });
       } catch (err) {
         console.error(err);
       }
     }
   }
-//이름으로 검색
-
+//이름으로 api 검색
+router.post('/namesearch',(req,res) => {
+    pokeapi.Pokenamesearch(req.body.name).then(result =>{
+        res.status(200).send(result);
+    }).catch(err => {
+        res.status(401).send(err);
+    })
+})
 module.exports = {router,save};
