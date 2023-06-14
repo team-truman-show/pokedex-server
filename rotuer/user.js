b4d028e9254764a11a67cc93bd150b19521f6d39
gusn98 <ah529@naver.com>
2023-06-14 18:57:19 +0900

    비밀번호찾기(변경)기능 추가

--- rotuer/user.js
+++ rotuer/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const login = require("../service/login");
 const signup = require("../service/signup");
 const update = require("../service/update");
+const change = require("../service/find");
 //로그인
 router.post("/login", (req, res) => {
   const userid = req.body.userid;
@@ -32,11 +33,23 @@ router.patch("/update", (req, res) => {
   const newNickname = req.body.nickname;
   update(userid, newNickname)
     .then((result) => {
-      console.log(result);
       res.status(200).send(result);
     })
     .catch((err) => {
       res.status(401).send(err);
     });
 });
+//비밀번호 찾기(변경)
+router.patch("/find", async (req, res) => {
+  const { nickname, userid, newPassword } = req.body;
+
+  try {
+    const result = await change(nickname, userid, newPassword);
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(401).send(err.message);
+  }
+});
+
 module.exports = router;
