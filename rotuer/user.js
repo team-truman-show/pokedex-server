8b98cd9b4d5cb906c35dc6b75b024352eb387e5a
gusn98 <ah529@naver.com>
2023-06-14 16:01:34 +0900

    fix:닉네임변경함수 분리

--- rotuer/user.js
+++ rotuer/user.js
@@ -1,24 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const login = require('../service/login');
-const signup = require('../service/signup');
+const login = require("../service/login");
+const signup = require("../service/signup");
+const update = require("../service/update");
 //로그인
-router.post('/login',(req,res)=> {
-    const userid = req.body.userid;
-    const password = req.body.password;
-    login(userid,password).then((result) => {
-        res.set("token", result);
-        res.status(200).send(result);
-    }).catch(err => {
-        res.status(401).send(err);
+router.post("/login", (req, res) => {
+  const userid = req.body.userid;
+  const password = req.body.password;
+  login(userid, password)
+    .then((result) => {
+      res.set("token", result);
+      res.status(200).send(result);
     })
-})
+    .catch((err) => {
+      res.status(401).send(err);
+    });
+});
 //회원가입
-router.post('/signup',(req,res) =>{
-    signup(req.body).then((result) => {
-        res.status(200).send(result);
-    }).catch(err => {
-        res.status(401).send(err);
+router.post("/signup", (req, res) => {
+  signup(req.body)
+    .then((result) => {
+      res.status(200).send(result);
     })
-})
+    .catch((err) => {
+      res.status(401).send(err);
+    });
+});
+//닉네임 변경
+router.patch("/update", (req, res) => {
+  const userid = req.body.userid;
+  const newNickname = req.body.nickname;
+  update(userid, newNickname)
+    .then((result) => {
+      console.log(result);
+      res.status(200).send(result);
+    })
+    .catch((err) => {
+      res.status(401).send(err);
+    });
+});
 module.exports = router;
