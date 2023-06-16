78e7d56d9b55247ec78e2af6bfa5aec31e96cce2
gusn98 <ah529@naver.com>
2023-06-16 11:02:12 +0900

    add:포켓몬 전체조회 추가

---
+++ service/searchAll.js
@@ -0,0 +1,16 @@
+const Pokemon = require("../models/pokemons");
+
+async function searchAll() {
+  try {
+    const pokemons = await Pokemon.findAll({
+      attributes: { exclude: ["imagegif"] },
+      //attributes-조회할 지정   exclude-제외
+    });
+
+    return pokemons;
+  } catch (err) {
+    return err.message;
+  }
+}
+
+module.exports = searchAll;
